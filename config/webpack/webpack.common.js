171ba56933bfeda88550b26c8956127eaa962be6
Oscar Sánchez <ojsm45@gmail.com>
2020-08-15 18:48:43 -0300

    Added advanced webpack config to handle dev and prod bundle

---
+++ config/webpack/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const SimpleProgressWebpackPlugin = require("simple-progress-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "../../src", "app.jsx"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../../build"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new SimpleProgressWebpackPlugin(),
+    new CleanWebpackPlugin({
+      dry: true,
+
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../../public", "index.html"),
+    }),
+  ],
+};
