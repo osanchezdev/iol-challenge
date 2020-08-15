23dac275105a7d2be38b0f4e6b8e9292a1cc8007
Oscar Sánchez <ojsm45@gmail.com>
2020-08-15 19:20:42 -0300

    Added styles, sass loadeers to webpack and more production ready plugins

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,4 +1,7 @@
-const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
@@ -7,4 +10,27 @@ module.exports = merge(common, {
   output: {
     filename: "bundle.[hash].min.js",
   },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css",
+    }),
+  ],
 });
--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "./styles/index.scss";
+
 const App = () => {
   return <h1>Hello Wepack!</h1>;
 };
