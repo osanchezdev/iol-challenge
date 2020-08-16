4ed488f1fbd7f51c4ef6b3ff87069de15ea3cb0e
Oscar Sánchez <ojsm45@gmail.com>
2020-08-16 16:01:03 -0300

    Replace loadable/components with React.lazy

--- src/components/global/GnomeCardsList.jsx
+++ src/components/global/GnomeCardsList.jsx
@@ -1,30 +1,31 @@
-import React, {useContext} from 'react';
-import loadable from '@loadable/component';
+import React, {Suspense, useContext} from 'react';
 import {Row, Col} from 'react-bootstrap';
 import {GnomesContext} from '../../context/gnomesContext';
 import GnomeCard from './GnomeCard';
 import Loader from './Loader';
 
-const LazyNoResults = loadable(() => import('./NoResults'));
+const LazyNoResults = React.lazy(() => import('./NoResults'));
 
 const GnomeCardsList = () => {
   const {filteredGnomes} = useContext(GnomesContext);
   return (
-    <Row>
-      {filteredGnomes ? (
-        filteredGnomes.length ? (
-          filteredGnomes.map(gnome => (
-            <Col key={gnome.id} xs={6} sm={6} xl={3} md={3}>
-              <GnomeCard {...gnome} />
-            </Col>
-          ))
+    <Suspense fallback={<Loader />}>
+      <Row>
+        {filteredGnomes ? (
+          filteredGnomes.length ? (
+            filteredGnomes.map(gnome => (
+              <Col key={gnome.id} xs={6} sm={6} xl={3} md={3}>
+                <GnomeCard {...gnome} />
+              </Col>
+            ))
+          ) : (
+            <LazyNoResults />
+          )
         ) : (
-          <LazyNoResults />
-        )
-      ) : (
-        <Loader />
-      )}
-    </Row>
+          <Loader />
+        )}
+      </Row>
+    </Suspense>
   );
 };
 
