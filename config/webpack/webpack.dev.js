936010af07ae65239fecd55176d4290f7ac5ca89
Oscar Sánchez <ojsm45@gmail.com>
2020-08-16 15:44:40 -0300

    Webpack devserver remove warnings for test purposes

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -26,6 +26,9 @@ module.exports = merge(common, {
     contentBase: path.resolve(__dirname, '../../build'),
     port: 8888,
     historyApiFallback: true,
+    stats: {
+      warnings: false,
+    },
   },
   devtool: 'source-map',
 });
