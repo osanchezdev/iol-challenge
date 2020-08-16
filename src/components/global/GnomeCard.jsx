f681e3415fa02d0bb6c8ba69dbbff78706dc8cdc
Oscar Sánchez <ojsm45@gmail.com>
2020-08-15 23:58:43 -0300

    Add lazy load to card images

--- src/components/global/GnomeCard.jsx
+++ src/components/global/GnomeCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {string} from 'prop-types';
+import {LazyLoadImage} from 'react-lazy-load-image-component';
 import '../../styles/components/global/GnomeCard.scss';
 
 const GnomeCard = ({name, thumbnail}) => {
   return (
     <div className="gnome-card--wrapper">
       <div className="gnome-card--image-wrapper">
-        <img src={thumbnail} alt={name} title={name} className="gnome-card--image" />
+        <LazyLoadImage alt={name} title={name} height={168} src={thumbnail} width={'100%'} />
       </div>
       <div className="gnome-card--title">
         <p>{name} </p>
