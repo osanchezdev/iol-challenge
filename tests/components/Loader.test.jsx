6b3ea5c930468e26708b19b80b3f52c3a9a49e8e
Oscar Sánchez <ojsm45@gmail.com>
2020-08-16 22:31:07 -0300

    Fix typo

--- tests/components/Loader.test.jsx
+++ tests/components/Loader.test.jsx
@@ -3,7 +3,7 @@ import {shallow} from 'enzyme';
 import Loader from '../../src/components/global/Loader';
 
 describe('Tests on <Loader />', () => {
-  test('v must be rendered correctly', () => {
+  test('<Loader /> must be rendered correctly', () => {
     const wrapper = shallow(<Loader />);
 
     expect(wrapper).toMatchSnapshot();
